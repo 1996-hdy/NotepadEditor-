7361eb50d2267fc239ee5a6017046d45e8b3c57b
unknown <2822708955@qq.com>
2020-08-03 22:01:12 +0800

    删除后重排

--- script.js
+++ script.js
@@ -1,127 +1,122 @@
 
+//过滤时间
 Vue.filter('date', time => moment(time).format('DD/MM/YY, HH:mm'))
 
 // New VueJS instance
 new Vue({
   name: 'notebook',
 
-  // CSS selector of the root DOM element
+  // vue实例挂载的元素
   el: '#notebook',
 
-  // Some data
   data () {
     return {
-      // These are loaded from localStorage and have a default value
-      // Don't forget the JSON parsing for the notes array
+      // 渲染页面 加载缓存中数据
       notes: JSON.parse(localStorage.getItem('notes')) || [],
+	  //是否上一次有选中
       selectedId: localStorage.getItem('selected-id') || null,
     }
   },
 
-  // Computed properties
+ 
   computed: {
+	  //点击选中 返回选中笔记
     selectedNote () {
-      // We return the matching note with selectedId
       return this.notes.find(note => note.id === this.selectedId)
     },
 
     notePreview () {
-      // Markdown rendered to HTML
+     //展示
       return this.selectedNote ? marked(this.selectedNote.content) : ''
     },
-
+// 创建时间越早越在前  点击收藏放在列表前面 
     sortedNotes () {
       return this.notes.slice().sort((a, b) => a.created - b.created)
+	  //0 位置不变 1 a在b后面 -1 a在b的前面
       .sort((a, b) => (a.favorite === b.favorite)? 0 : a.favorite? -1 : 1)
     },
 
     linesCount () {
       if (this.selectedNote) {
-        // Count the number of new line characters
+       //统计行数
         return this.selectedNote.content.split(/\r\n|\r|\n/).length
       }
     },
-
+  //统计字数
     wordsCount () {
       if (this.selectedNote) {
         var s = this.selectedNote.content
-        // Turn new line cahracters into white-spaces
         s = s.replace(/\n/g, ' ')
-        // Exclude start and end white-spaces
         s = s.replace(/(^\s*)|(\s*$)/gi, '')
-        // Turn 2 or more duplicate white-spaces into 1
         s = s.replace(/[ ]{2,}/gi, ' ')
-        // Return the number of spaces
         return s.split(' ').length
       }
-    },
-
-    charactersCount () {
-      if (this.selectedNote) {
-        return this.selectedNote.content.split('').length
-      }
-    },
+    }
   },
 
-  // Change watchers
   watch: {
-    // When our notes change, we save them
     notes: {
-      // The method name
       handler: 'saveNotes',
-      // We need this to watch each note's properties inside the array
+      //监听数组中的每一项的对象属性的具体变化
       deep: true,
     },
-    // Let's save the selection too
+    //缓存选中的笔记
     selectedId (val, oldVal) {
       localStorage.setItem('selected-id', val)
     },
   },
 
   methods:{
-    // Add a note with some default content and select it
+    // 添加笔记
     addNote () {
       const time = Date.now()
       // Default new note
       const note = {
         id: String(time),
-        title: 'New note ' + (this.notes.length + 1),
-        content: '**Hi!** This notebook is using [markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) for formatting!',
+        // title: '笔记' + (this.notes.length + 1),
+        content: '来来来~记录学习咯',
         created: time,
+		order:this.notes.length + 1,
         favorite: false,
       }
-      // Add
+      // 添加
       this.notes.push(note)
-      // Select
+      //选中
       this.selectNote(note)
     },
 
-    // Remove the selected note and select the next one
+    // 删除
     removeNote () {
       if (this.selectedNote && confirm('Delete the note?')) {
-        // Remove the note in the notes array
         const index = this.notes.indexOf(this.selectedNote)
-        if (index !== -1) {
-          this.notes.splice(index, 1)
-        }
+       let length = this.notes.length;
+       if (index == -1) {
+       	return
+       }
+       if (index + 1 < length) {
+       	// 修改i后面的选中的序号 减1就行
+       	for (let j = index; j < length; j++) {
+       		console.log(this.notes[j])
+       		this.notes[j].order--
+       	}
+       }
+       this.notes.splice(index, 1)
+		
       }
     },
 
     selectNote (note) {
-      // This will update the 'selectedNote' computed property
       this.selectedId = note.id
     },
 
     saveNotes () {
-      // Don't forget to stringify to JSON before storing
+      // 不要忘记localStorage存放的是字符串
       localStorage.setItem('notes', JSON.stringify(this.notes))
       console.log('Notes saved!', new Date())
     },
-
+    //选中取消收藏
     favoriteNote () {
-      // this.selectedNote.favorite = !this.selectedNote.favorite
-      // this.selectedNote.favorite = this.selectedNote.favorite ^ true
-      this.selectedNote.favorite ^= true
+      this.selectedNote.favorite = !this.selectedNote.favorite
     },
   },
 })
